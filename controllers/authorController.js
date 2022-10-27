1a7b41083472a768b2bce6c06eadae3ec9b543db
erynder-z <stfn.bgr@gmail.com>
2022-10-27 20:58:43 +0200

    Add author update controller

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -196,11 +196,102 @@ exports.author_delete_post = (req, res, next) => {
 };
 
 // Display Author update form on GET.
-exports.author_update_get = (req, res) => {
-    res.send('NOT IMPLEMENTED: Author update GET');
+exports.author_update_get = (req, res, next) => {
+    // Get book, authors and genres for form.
+    async.parallel(
+        {
+            author(callback) {
+                Author.findById(req.params.id).exec(callback);
+            },
+            authors_books(callback) {
+                Book.find({ author: req.params.id }, 'title summary').exec(
+                    callback
+                );
+            },
+        },
+        (err, results) => {
+            if (err) {
+                return next(err);
+            }
+            if (results.author == null) {
+                // No results.
+                const err = new Error('Author not found');
+                err.status = 404;
+                return next(err);
+            }
+            // Success.
+            res.render('author_form', {
+                title: 'Update  Author',
+                author: results.author,
+                author_books: results.authors_books,
+            });
+        }
+    );
 };
 
 // Handle Author update on POST.
-exports.author_update_post = (req, res) => {
-    res.send('NOT IMPLEMENTED: Author update POST');
-};
+exports.author_update_post = [
+    // Validate and sanitize fields.
+    body('first_name')
+        .trim()
+        .isLength({ min: 1 })
+        .escape()
+        .withMessage('First name must be specified.')
+        .isAlphanumeric()
+        .withMessage('First name has non-alphanumeric characters.'),
+    body('family_name')
+        .trim()
+        .isLength({ min: 1 })
+        .escape()
+        .withMessage('Family name must be specified.')
+        .isAlphanumeric()
+        .withMessage('Family name has non-alphanumeric characters.'),
+    body('date_of_birth', 'Invalid date of birth')
+        .optional({ checkFalsy: true })
+        .isISO8601()
+        .toDate(),
+    body('date_of_death', 'Invalid date of death')
+        .optional({ checkFalsy: true })
+        .isISO8601()
+        .toDate(),
+
+    // Process request after validation and sanitization.
+    (req, res, next) => {
+        // Extract the validation errors from a request.
+        const errors = validationResult(req);
+
+        // Create a BookInstance object with escaped and trimmed data.
+        const author = new Author({
+            first_name: req.body.first_name,
+            family_name: req.body.family_name,
+            date_of_birth: req.body.date_of_birth,
+            date_of_death: req.body.date_of_death,
+            _id: req.params.id, //This is required, or a new ID will be assigned!
+        });
+
+        if (!errors.isEmpty()) {
+            // There are errors. Render form again with sanitized values and error messages.
+            res.render('author_form', {
+                title: 'Update Author',
+                author: author,
+                errors: errors.array(),
+            });
+            return;
+        }
+
+        // Data from form is valid. Update the record.
+        Author.findByIdAndUpdate(
+            req.params.id,
+            author,
+            {},
+            (err, theauthor) => {
+                if (err) {
+                    return next(err);
+                }
+
+                // Successful: redirect to book detail page.
+                res.redirect(theauthor.url);
+            }
+        );
+    },
+];
